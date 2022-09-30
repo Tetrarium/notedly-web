1510e2c34bf035e7ab8dcacc14b8c747d5b354cd
Alexandr Turov <alexandr985crv@gmail.com>
2022-09-30 23:01:35 +0700

    Chapter 12. Building a web client with React

---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Импортируем компонент Link из react-router
+import { Link } from "react-router-dom";
+
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+	return (
+		<div>
+			<Header />
+			<Navigation />
+			<p>This is the home page</p>
+		</div>
+	);
+};
+
+export default Home;
